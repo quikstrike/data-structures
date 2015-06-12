a0ebec8d55577f79d9193a34c8f119b1dd5217a3
quikstrike <quikstrik3@gmail.com>
2015-06-12 12:38:02 -0500

    graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,54 +1,58 @@
 
 
 var Graph = function(){
-  this.nodes = {}
-  this.lastNodeName = 0
+  this.nodeList = {}
+
 
 };
-var _Node = function(val) {
-  this.value = val
-  this.edges = []
-  this.name = Graph.prototype.namingFunc()
+
+var _Node = function(value) {
+  var newNode = {}
+  newNode.edges = []
+  newNode.val = value
+  return newNode
 }
 
-Graph.prototype.namingFunc = function() {
-  var nodeName = this.lastNodeName
-  this.lastNodeName++
-  return nodeName
-
-}
 Graph.prototype.addNode = function(node){
-  //just adds it to list of nodes
-  this.nodes[_Node(node)]
-  console.log(_Node[node])
+  this.nodeList[node] = _Node(node)
 
 };
 
 Graph.prototype.contains = function(node){
-  var foundStatus = false
-  _.each(this.nodes, function(item){
-    if (item === node) {
-      foundStatus = true
-    }
-  })
-  return foundStatus
+  if(this.nodeList[node] === undefined){
+    return false
+  }
+
+  return (this.nodeList[node].val === node)
 };
 
 Graph.prototype.removeNode = function(node){
-  this.nodes.pop(node)
+  if (this.contains(node)) {
+    delete this.nodeList[node]
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return (this.nodeList[fromNode].edges.indexOf(toNode) != -1 && this.nodeList[toNode].edges.indexOf(fromNode) != -1)
 
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodeList[fromNode].edges.push(toNode)
+  this.nodeList[toNode].edges.push(fromNode)
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
+  this.nodeList[fromNode].edges.pop(toNode)
+  this.nodeList[toNode].edges.pop(fromNode)
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.nodeList, function(n){
+    cb(n.val);
+  })
 };
 
 /*
